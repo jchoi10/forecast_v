fa283a2e05f696c5d70cecaa239d5eda507a1211
jchoi10 <noreply>
2022-10-28 00:43:10 -0400

    set up starter code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # forecast_v
+
+## User Story
+
+
+
+## Acceptance Criteria
+
+
+
+## Live URL
+
+- [https://jchoi10.github.io/schedule_sorter/](https://jchoi10.github.io/schedule_sorter/)
+
+# Area of improvemnet
+
+- 
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var APIkey = "c5983aad19183fb683432bc8f6baf7fc"
+
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// api call
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
