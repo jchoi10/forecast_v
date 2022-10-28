4ea84ceeac0af8225aeb09adf8a6af7d13239e7a
jchoi10 <noreply>
2022-10-28 01:36:13 -0400

    set up html code for search area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var day = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").append(day);
+
 var APIkey = "c5983aad19183fb683432bc8f6baf7fc"
 
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
