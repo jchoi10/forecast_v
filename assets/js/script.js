0c3c3a46de9bd04b99cba31ef2839f0d8dd28bd4
jchoi10 <noreply>
2022-10-29 00:30:42 -0400

    local storage use for saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var day = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(day);
 
 var APIkey = "c5983aad19183fb683432bc8f6baf7fc"
+var previousHistorylist = [];
 
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
